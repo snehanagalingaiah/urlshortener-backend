57c088a548d6ef2cfff723a0a3dd3d7b8d9e3924
snehanagalingaiah <snehanagalingaiah@gmail.com>
2023-06-13 06:51:16 +0530

    Update authenticate.js
    correcting indentation

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,27 +1,23 @@
 const jwt = require("jsonwebtoken");
 
+//Middleware to fetch access token from local storage and verify it
 
 const authenticate = async (req,res,next) =>{
+       console.log("API authenticate")
+      // Check whether access token exists in headers
+        if (!req.headers['access-token'])
+           return res.status(400).send( "Token not found" );
 
-  console.log("API authenticate")
-
-	// Check whether access token exists in headers
-     if (!req.headers['access-token'])
-    return res.status(400).send( "Token not found" );
-
-   //verify token
-    try {
-    const user = jwt.verify(req.headers['access-token'], process.env.JWT_SECRET);
-    req.body.currentUser= user;
-    next();
-
-  } 
-  catch (err) {
-    console.error(err);
-    return res.status(400).send( "Unauthorised" );
-  }
-
-
-}
+      //verify token
+         try {
+             const user = jwt.verify(req.headers['access-token'], process.env.JWT_SECRET);
+              req.body.currentUser= user;
+             next();
+            } 
+         catch (err) {
+             console.error(err);
+             return res.status(400).send( "Unauthorised" );
+          }
+ }
 
 module.exports = authenticate;
